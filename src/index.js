990ccc08dbc1d77efc4ea2bb78e92e77ebacacda
jarguello1 <jcarguello11@live.com>
2022-09-13 22:20:27 -0400

    add text to submit button, clean index.js

--- src/index.js
+++ src/index.js
@@ -10,15 +10,11 @@ let humidity;
 async function getWeather() {
     let response = await fetch(url);
     const weatherData = await response.json();
-    console.log(weatherData.coord);
-    console.log(weatherData.main);
+    console.log(weatherData);
     temp =  (1.8*(weatherData.main.temp-273) + 32).toFixed(1);
     condition = weatherData.weather[0].main;
     humidity = weatherData.main.humidity;
     feelsLike = (1.8*(weatherData.main.feels_like-273) + 32).toFixed(1);
-
-    console.log(feelsLike);
-    
 }
 
 getWeather();
