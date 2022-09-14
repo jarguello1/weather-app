70b9d4208e64c11e036a75bb04abbf7491cbf4cc
jarguello1 <jcarguello11@live.com>
2022-09-13 22:17:10 -0400

    assign main weather aspects to variables in F deg

--- main.js
+++ main.js
@@ -15,7 +15,7 @@
   \**********************/
 /***/ (() => {
 
-eval("const myKey = '0e699be9156d5e418f4bde0cd73827d5';\nlet location = 'London';\n\nlet url = `http://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=${myKey}`\n\nasync function getWeather() {\n    let response = await fetch(url);\n    const weatherData = await response.json();\n    console.log(weatherData.coord);\n    console.log(weatherData.main);\n    console.log(weatherData);\n}\n\ngetWeather();\n\n//# sourceURL=webpack://weather-app/./src/index.js?");
+eval("const myKey = '0e699be9156d5e418f4bde0cd73827d5';\nlet location = 'Miami';\n\nlet url = `http://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=${myKey}`\nlet temp;\nlet condition;\nlet feelsLike;\nlet humidity;\n\nasync function getWeather() {\n    let response = await fetch(url);\n    const weatherData = await response.json();\n    console.log(weatherData.coord);\n    console.log(weatherData.main);\n    temp =  (1.8*(weatherData.main.temp-273) + 32).toFixed(1);\n    condition = weatherData.weather[0].main;\n    humidity = weatherData.main.humidity;\n    feelsLike = (1.8*(weatherData.main.feels_like-273) + 32).toFixed(1);\n\n    console.log(feelsLike);\n    \n}\n\ngetWeather();\n\nasync function logWeather() {\n\n}\n\n\nfunction Weather(temp, condition) {\n    \n}\n\n//# sourceURL=webpack://weather-app/./src/index.js?");
 
 /***/ })
 
